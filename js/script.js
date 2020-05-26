85af3637cb8b3a1df10ab72b78ea61d42730dfc8
LazolMan <31419412+LazolMan@users.noreply.github.com>
2020-05-26 07:23:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,86 @@
+jQuery(function ($) {
+  "use strict";
+
+  var sections = $("section");
+  var nav = $("#header");
+  var nav_height = nav.outerHeight();
+
+  $(window).on("scroll", function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function () {
+      var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find("a").removeClass("active");
+        sections.removeClass("active");
+
+        $(this).addClass("active");
+        nav.find('a[href="#' + $(this).attr("id") + '"]').addClass("active");
+      }
+    });
+  });
+
+  nav.find("a").on("click", function () {
+    var $el = $(this),
+      id = $el.attr("href");
+
+    $("html, body").animate(
+      {
+        scrollTop: $(id).offset().top - nav_height,
+      },
+      750
+    );
+
+    return false;
+  });
+
+  window.onscroll = function () {
+    fixHeader();
+  };
+
+  var header = document.getElementById("header");
+  var sticky = header.offsetTop;
+  
+  function fixHeader() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
+
+  if ($(".open_video").length) {
+    $(".open_video").magnificPopup({
+      disableOn: 700,
+      type: "iframe",
+      mainClass: "mfp-fade",
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false,
+    });
+  }
+
+  if ($(".link").length) {
+    $(".link").magnificPopup({
+      type: "image",
+      closeOnContentClick: true,
+      mainClass: "mfp-img-mobile",
+      image: {
+        verticalFit: true,
+      },
+    });
+  }
+
+  if ($(".reviews_slider").length) {
+    $(".reviews_slider").slick({
+      arrows: false,
+      dots: true,
+      autoplay: false,
+      autoplaySpeed: 5000,
+      swipeToSlide: true,
+    });
+  }
+});
